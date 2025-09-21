32c48680ec1096f09b0fe315ce2483c7e322b9b2
narasimha <gnr.2k21@gmail.com>
2025-09-21 15:37:35 +0530

    conversation

---
+++ .sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Ram: Hi Sam, how are you?"
+echo "Sam: Hi Ram, I'm good. How about you?"
+echo "Ram: I'm fine too. What are you doing today?"
+echo "Sam: I'm learning shell scripting."
+echo "Ram: That's great! Keep it up."
+echo "Sam: Thank you, Ram!"
+
