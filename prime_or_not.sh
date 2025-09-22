971bdf7207b37947bc1ba272db4fa01b047ce965
narasimha <gnr.2k21@gmail.com>
2025-09-23 00:05:48 +0530

    variables

---
+++ prime_or_not.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Enter a number: " num
+
+if [ "$num" -le 1 ]; then
+    echo "$num is not a prime number."
+    exit 0
+fi
+
+is_prime=1
+for ((i=2; i<=num/2; i++))
+do
+    if (( num % i == 0 )); then
+        is_prime=0
+        break
+    fi
+done
+
+if [ $is_prime -eq 1 ]; then
+    echo "$num is a prime number."
+else
+    echo "$num is not a prime number."
+fi
