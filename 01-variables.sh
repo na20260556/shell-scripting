4b709fc57d590ce819017e230f3bfda1a95aaf65
narasimha <gnr.2k21@gmail.com>
2025-09-22 22:11:20 +0530

    variables

---
+++ 01-variables.sh
@@ -0,0 +1,11 @@
+
+
+!#/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: Hi $PERSON1, I'm good. How about you?"
+echo "$PERSON1: I'm fine too. What are you doing today?"
+echo "$PERSON2: I'm learning shell scripting."
